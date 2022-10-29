b0daa25b14ec0bd6513886b238bcc7f7b235b2a4
DongHeon Lee <niche3600@gmail.com>
2022-10-29 22:06:03 +0900

    本番環境用の設定を修正、開発環境用のDockerfileを作成
    - 本番環境
      - volumesを指定する必要がないため、削除
      - `script` 、 `config` はビルド時には必要なので、Dockerfile内でCOPYさせる
      - `bin/prod_rebuild.sh` のvolume削除処理をコメントアウト
    - 開発環境
      - 本番環境用と兼用して使っていたため、開発環境用のDockerfileを用意

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,3 @@
 FROM postgres:14.5-alpine
+COPY ./script /docker-entrypoint-initdb.d
+COPY ./config/postgresql.conf /etc/postgresql/postgresql.conf
--- bin/prod_rebuild.sh
+++ bin/prod_rebuild.sh
@@ -4,10 +4,10 @@ set -eu
 
 CONTAINERNAME="postgres"
 IMAGENAME="prod/postgres"
-VOLUMENAME="db-store"
-DIRECTORYNAME="`pwd`"
-DIRECTORYNAME="`basename -- "$DIRECTORYNAME"`"
-VOLUMENAME=$DIRECTORYNAME"_"$VOLUMENAME
+# VOLUMENAME="db-store"
+# DIRECTORYNAME="`pwd`"
+# DIRECTORYNAME="`basename -- "$DIRECTORYNAME"`"
+# VOLUMENAME=$DIRECTORYNAME"_"$VOLUMENAME
 
 if docker compose ps ${CONTAINERNAME} | grep -q running; then
   cmd="docker compose down"
@@ -18,9 +18,9 @@ if docker compose ps ${CONTAINERNAME} | grep -q running; then
   echo $cmd
   $cmd
 
-  cmd="docker volume rm ${VOLUMENAME}"
-  echo $cmd
-  $cmd
+  # cmd="docker volume rm ${VOLUMENAME}"
+  # echo $cmd
+  # $cmd
 else
   if docker images | grep -q ${IMAGENAME}; then
     cmd="docker image rm ${IMAGENAME}"
@@ -28,11 +28,11 @@ else
     $cmd
   fi
 
-  if docker volume list | grep -q ${IMAGENAME}; then
-    cmd="docker volume rm ${VOLUMENAME}"
-    echo $cmd
-    $cmd
-  fi
+  # if docker volume list | grep -q ${IMAGENAME}; then
+  #   cmd="docker volume rm ${VOLUMENAME}"
+  #   echo $cmd
+  #   $cmd
+  # fi
 
   cmd="docker compose -f docker-compose.yml up -d"
   echo $cmd
